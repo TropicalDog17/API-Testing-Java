beb5752470b6b172c36b3e38a32bd60ede56368f
NguyenMinhAn <anpro200213@gmail.com>
2022-07-23 16:09:47 +0700

    an

--- src/main/java/utils/Utility.java
+++ src/main/java/utils/Utility.java
@@ -156,6 +156,31 @@ public class Utility {
             }
             return sb.toString();
         }
+    public static String getRandomPhone(int length) {
+        // create a string of uppercase and lowercase characters and numbers
+
+        String numbers = "0123456789";
+        // combine all strings
+        String alphaNumeric = numbers;
+        // create random string builder
+        StringBuilder sb = new StringBuilder();
+        // create an object of Random class
+        Random random = new Random();
+        // specify length of random string
+        for (int i = 0; i < length; i++) {
+
+            // generate random index number
+            int index = random.nextInt(alphaNumeric.length());
+
+            // get character specified by index
+            // from the string
+            char randomChar = alphaNumeric.charAt(index);
+
+            // append the character to string builder
+            sb.append(randomChar);
+        }
+        return sb.toString();
+    }
 
         private static void displayMenu() {
             System.out.println("==============MENU==============");
